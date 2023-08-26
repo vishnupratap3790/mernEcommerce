86213b9c7270228a4e9c003120600ff9c34d6d8d
vishnupratap3790 <pratapvishnu2001@gmail.com>
2023-08-26 18:04:24 +0530

    update4 commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true, useFindAndModify:true
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true
     }).then((data)=>{
             console.log(`mongodb connected with server ${data.connection.host}`);
         });
